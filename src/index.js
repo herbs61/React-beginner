d9937ff839a90aef6122a0d74d283162a718d694
Herbs <herbertowusu61@gmail.com>
2025-08-12 22:34:27 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -101,7 +101,7 @@ function Footer() {
     //     }
     return (
         <footer className="footer">
-            {new Date().toLocaleTimeString()}.We're currently open
+            {new Date().toLocaleTimeString()}.We're 
 
         </footer>
     );
